6e61777587a323f6a8a31795c5f2026388d448e1
anariba-hn <anariba.hn@hotmail.com>
2018-05-02 16:00:32 -0600

    Modify script for back-end validations

--- setPayments.php
+++ setPayments.php
@@ -4,6 +4,7 @@ include("./connex.php");
 $user_address = $_POST['user_address'];
 $status = "pending"; //seting a default status
 $response = array();
+$succes = false;
 //$cnn  =  include variable
 
 if ($user_address != null) 
@@ -32,42 +33,53 @@ if ($user_address != null)
    	    $data2 = mysqli_fetch_row($result);
     	$wallet_balance = (int) $data2[0];
 
-		$query3 = "INSERT INTO vf_payments (payments_balance, payments_status, payments_wallet, payments_date) VALUES ('$wallet_balance', '$status', '$user_address',now()) ";
-		if (!$result = mysqli_query($cnn, $query3))
-		 exit(mysqli_error($cnn));
+    	if ($wallet_balance >= 50) //Validation stetment
+    	{
+			$query3 = "INSERT INTO vf_payments (payments_balance, payments_status, payments_wallet, payments_date) VALUES ('$wallet_balance', '$status', '$user_address',now()) ";
+			if (!$result = mysqli_query($cnn, $query3))
+			 exit(mysqli_error($cnn));
+			else{
+				$response['status'] = 200;
+		        $response['message'] = "Succes !";
+		        $succes = true;
+			}
+    	}
 		else{
-			$response['status'] = 200;
-	        $response['message'] = "Succes !";
-	        $succes = true;
+			$response['status'] = 404;
+	        $response['message'] = "You don't have enough money";
+	        $succes = falase;
 		}
 	
 	//Update the Balance
-
-	$query4 = "SELECT wallet_unlock FROM wallet WHERE user_id = '$userid'";
-	if (!$result = mysqli_query($cnn, $query4)) 
+	if ($succes) 
+	{
+		$query4 = "SELECT wallet_unlock FROM wallet WHERE user_id = '$userid'";
+		if (!$result = mysqli_query($cnn, $query4)) 
 		exit(mysqli_error($cnn));
 
 		$data3 = mysqli_fetch_row($result);
 		$old_balance = (int) $data3[0];
 		$new_balance = $old_balance + $wallet_balance;
 
-	$query5 = "SELECT wallet_withdraws FROM wallet WHERE user_id = '$userid'";
-	if (!$result = mysqli_query($cnn, $query5)) 
+		$query5 = "SELECT wallet_withdraws FROM wallet WHERE user_id = '$userid'";
+		if (!$result = mysqli_query($cnn, $query5)) 
 		exit(mysqli_error($cnn));
 
 		$data4 = mysqli_fetch_row($result);
 		$old_withdraws = (int) $data4[0];
 		$new_withdraws = $old_withdraws + 1;
 
-	$query6 = "UPDATE wallet SET wallet_balance = 0, wallet_unlock = '$new_balance', wallet_withdraws = '$new_withdraws' WHERE user_id = '$userid'";
-	if (!$result = mysqli_query($cnn,$query6)) 
+		$query6 = "UPDATE wallet SET wallet_balance = 0, wallet_unlock = '$new_balance', wallet_withdraws = '$new_withdraws' WHERE user_id = '$userid'";
+		if (!$result = mysqli_query($cnn,$query6)) 
 		exit(mysqli_error($cnn));
-	else{
+		else{
 			$response['status'] = 200;
 	        $response['message'] = "Succes !";
 	        $succes = true;
-	}
+		}
 
+	}
+	
 	header('Content-type: application/json; charset=utf8');
 	echo json_encode($response);
 	
