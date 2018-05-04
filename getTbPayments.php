2dc1fa1d82f4b5cd6ea83750fd7bbd14acb8a66f
anariba-hn <anariba.hn@hotmail.com>
2018-05-04 14:12:55 -0600

    New script to get Tb Payments

--- getTbPayments.php
+++ getTbPayments.php
@@ -1,5 +1,52 @@
 <?php 
 include("./connex.php");
+
+$request=$_REQUEST;
+$col =array(
+    0   =>  'id_payments',
+    1   =>  'payments_balance',
+    2   =>  'payments_status',
+    3   =>  'payments_wallet',
+    4   =>  'payments_date'
+);  //create column like table in database
+$sql ="SELECT * FROM vf_payments";
+$query=mysqli_query($con,$sql);
+$totalData=mysqli_num_rows($query);
+$totalFilter=$totalData;
+//Search
+$sql ="SELECT * FROM vf_payments WHERE 1=1";
+if(!empty($request['search']['value'])){
+    $sql.=" AND (idid_payments Like '".$request['search']['value']."%' ";
+    $sql.=" OR payments_status Like '".$request['search']['value']."%' ";
+    $sql.=" OR payments_wallet Like '".$request['search']['value']."%' ";
+    $sql.=" OR payments_date Like '".$request['search']['value']."%' )";
+}
+$query=mysqli_query($con,$sql);
+$totalData=mysqli_num_rows($query);
+//Order
+$sql.=" ORDER BY ".$col[$request['order'][0]['column']]."   ".$request['order'][0]['dir']."  LIMIT ".
+    $request['start']."  ,".$request['length']."  ";
+$query=mysqli_query($con,$sql);
+$data=array();
+while($row=mysqli_fetch_array($query)){
+    $subdata=array();
+    $subdata[]=$row[0]; //id
+    $subdata[]=$row[1]; //amount
+    $subdata[]=$row[2]; //status
+    $subdata[]=$row[3]; //wallet
+    $subdata[]=$row[4]; //date      
+    $data[]=$subdata;
+}
+$json_data=array(
+    "draw"              =>  intval($request['draw']),
+    "recordsTotal"      =>  intval($totalData),
+    "recordsFiltered"   =>  intval($totalFilter),
+    "data"              =>  $data
+);
+echo json_encode($json_data);
+
+
+/*
 $request = $_REQUEST;
 $col = array(
 	0 => 'id_payments',
@@ -11,9 +58,11 @@ $col = array(
 //$cnn = include 
 
 $query = "SELECT * FROM vf_paymetns";
-$result = mysqli_query($cnn, $query);
 
-$totalData = mysqli_num_rows($result);
+if (!$result = mysqli_query($cnn, $query))
+	exit(mysqli_error($cnn));
+
+$totalData = mysqli_fetch_rows($result);
 
 $totalFilter = $totalData;
 
@@ -38,7 +87,7 @@ $json_data = array(
 );
 
 header('Content-type: application/json; charset=utf8');
- json_encode($json_data);
+json_encode($json_data);
 
 
 /*
